1d174f4b182a05efc4e47735087513dd1e8ebc23
DashinIN <green19grant@yandex.ru>
2022-06-07 11:02:08 +0300

    Нормальный билд npm

---
+++ docs/script/paralax.js
@@ -0,0 +1,61 @@
+
+
+window.onload = function () {
+
+    const paralax = document.querySelector('.paralax');
+
+    if (paralax) {
+        const headImage = document.querySelector('.paralax__image');
+
+        const headText = document.querySelector('.hi__body');
+
+        const forImage = 40;
+        const forText = 50;
+        
+        const speed = 0.1;
+
+        let positionX = 0, positionY = 0;
+        let coordXpencent = 0, coordYpencent = 0;
+
+
+
+
+
+        function setMouseParalaxStyle() {
+            const distX = coordXpencent - positionX;
+            const distY = coordYpencent - positionY;
+
+            positionX+=distX*speed;
+            positionY+=distY*speed;
+
+            headImage.style.cssText = `transform: translate(${positionX/forImage}%, ${positionY/forImage}%);`;
+            headText.style.cssText = `transform: translate(${positionX/forText}%, ${positionY/forText}%);`;
+
+          requestAnimationFrame(setMouseParalaxStyle);
+
+        }
+
+        setMouseParalaxStyle();
+
+        paralax.addEventListener('mousemove', function(e) {
+
+            const paralaxWidth = paralax.offsetWidth;
+            const paralaxHeight = paralax.offsetHeight;
+
+            const coordX = e.pageX- paralaxWidth/2;
+            const coordY = e.pageY - paralaxHeight/2;
+
+            coordXpencent = coordX / paralaxWidth*100;
+            coordYpencent = coordY / paralaxHeight *100;
+            
+
+            console.log(coordX);
+            console.log(coordY);
+            console.log(coordXpencent);
+            console.log(coordYpencent);
+
+
+        })
+    }
+
+}
